b5291ca56e20e4bf38df800ba7a80c700c23b722
Herman Adu <herman.adu@hotmail.com>
2022-01-04 14:14:49 +0000

    Created complete project structure, added navbar, header, brand and whatGpt3 components

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Feature from "../../components/feature/Feature";
+import "./features.css";
+
+const Features = () => {
+  return <div>Features</div>;
+};
+
+export default Features;
